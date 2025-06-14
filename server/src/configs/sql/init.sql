2b715ec882871fe3489f117afa506d75d5faf322
Wolfyre243 <wolfyreblueflare@gmail.com>
2025-06-15 04:18:40 +0800

    [FEATURE] added prisma

---
+++ server/src/configs/sql/init.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+	user_id SERIAL PRIMARY KEY,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	password CHAR(60) NOT NULL,
+	is_verified BOOLEAN DEFAULT false,
+	is_deleted BOOLEAN DEFAULT false,
+	created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMPTZ NULL
+);
+
+CREATE TABLE userprofile (
+	profile_id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL,
+	is_highrisk BOOLEAN NOT NULL,
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL,
+	created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMPTZ NULL,
+
+	FOREIGN KEY (user_id)
+	REFERENCES users(user_id)
+		ON DELETE CASCADE
+);
