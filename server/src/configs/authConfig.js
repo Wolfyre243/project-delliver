4c96ee148c40c655e7ebeaa33628861854a38022
Wolfyre243 <wolfyreblueflare@gmail.com>
2025-06-19 17:08:42 +0800

    [FIX] updated cookieConfig

--- server/src/configs/authConfig.js
+++ server/src/configs/authConfig.js
@@ -1,8 +1,9 @@
 const cookieOptions = {
   path: '/',
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production", // true in production
-  sameSite: "lax",
-};
+  secure: process.env.NODE_ENV === 'production', // true in production
+  sameSite: 'lax',
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+}
 
-export default cookieOptions;
+export default cookieOptions
