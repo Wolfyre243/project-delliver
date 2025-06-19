cd6ebc02dd13c27501097aaaff230cd393786749
Wolfyre243 <wolfyreblueflare@gmail.com>
2025-06-19 18:28:46 +0800

    [FIX] updated cookie config for testing

--- server/src/configs/authConfig.js
+++ server/src/configs/authConfig.js
@@ -1,8 +1,12 @@
 const cookieOptions = {
   path: '/',
   httpOnly: true,
+  domain:
+    process.env.NODE_ENV === 'production'
+      ? 'project-delliver.onrender.com'
+      : '',
   secure: process.env.NODE_ENV === 'production', // true in production
-  sameSite: 'lax',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   maxAge: 30 * 24 * 60 * 60 * 1000,
 }
 
