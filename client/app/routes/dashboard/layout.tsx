5389239a64c97f4ebee56af8ec4c987a2eb06a11
Shanna024985 <Shannalee1307@outlook.com>
2025-06-20 16:43:35 +0800

    [FEATURE] added a dark mode light mode button and placeholder breadcrumb

--- client/app/routes/dashboard/layout.tsx
+++ client/app/routes/dashboard/layout.tsx
@@ -7,10 +7,24 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from '~/components/ui/sidebar'
+import {
+  Sun,
+  ChevronDown,
+  MoonIcon
+} from "lucide-react"
 import { Separator } from '~/components/ui/separator'
 import RequireAuth from '~/components/RequireAuth'
-
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
+import { Button } from '~/components/ui/button'
+import { DropdownMenuShortcut } from '~/components/ui/dropdown-menu'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '~/components/ui/select'
+import LayoutBreadcrumb from './layoutBreadcrumb'
+import { useEffect, useState } from 'react'
 export default function Layout({ children }: { children: React.ReactNode }) {
+  let [windowHref, setWindowHref] = useState("");
+  useEffect(()=>{
+    setWindowHref(window.location.href)
+  },[])
   return (
     <>
       <RequireAuth>
@@ -19,13 +33,29 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-              <header className="flex h-18 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-18">
+              <header className="flex h-18 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-18 justify-between">
                 <div className="flex items-center gap-2 px-4">
-                  <SidebarTrigger />
-                  <Separator
+                    <div className='flex gap-2 flex-wrap items-center'>
+                      <SidebarTrigger />
+                      <Separator
                     orientation="vertical"
                     className="mr-2 data-[orientation=vertical]:h-4"
-                  />
+                      />
+                      <LayoutBreadcrumb href={windowHref}/>
+                    </div>
+
+                </div>
+                <div className='mr-6'>
+                  <Select>
+                    <SelectTrigger>
+                      <SelectValue placeholder={<MoonIcon/>}></SelectValue>
+                    </SelectTrigger>
+                    <SelectContent className='w-fit'>
+                      <SelectItem className='w-fit' value='light mode'><Sun></Sun></SelectItem>
+                      <SelectItem value='dark mode' className='w-fit'><MoonIcon></MoonIcon></SelectItem>
+
+                    </SelectContent>
+                  </Select>
                 </div>
               </header>
               <Outlet />
---
+++ client/app/routes/dashboard/layoutBreadcrumb.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from '~/components/ui/breadcrumb'
+
+const LayoutBreadcrumb = (props:{href: any}) => {
+  return (
+    <>
+        <Breadcrumb>
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href={props.href}>
+                        ad
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator/>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href={props.href}>
+                        ad
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    </>
+  )
+}
+
+export default LayoutBreadcrumb
