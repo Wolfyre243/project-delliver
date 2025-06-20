3f6b2cf2462dfb94be5525cefc032defc82b08f2
Wolfyre243 <wolfyreblueflare@gmail.com>
2025-06-21 01:56:48 +0800

    [FEATURE] added mission tables

--- server/src/configs/sql/procedures.sql
+++ server/src/configs/sql/procedures.sql
@@ -5,7 +5,6 @@ CREATE OR REPLACE PROCEDURE register_user(
 	p_password CHAR(60),
 	p_fname VARCHAR(100),
 	p_lname VARCHAR(100),
-	p_is_highrisk BOOLEAN,
 	p_exercise_duration VARCHAR(255),
 	p_history TEXT[],
 	o_user_id OUT INTEGER 
@@ -22,8 +21,8 @@ BEGIN
 	RETURNING user_id INTO new_user_id;
 
 	INSERT INTO userprofile
-	(user_id, first_name, last_name, is_highrisk, exercise_duration, history) VALUES
-	(new_user_id, p_fname, p_lname, p_is_highrisk,p_exercise_duration,p_history)
+	(user_id, first_name, last_name, exercise_duration, history) VALUES
+	(new_user_id, p_fname, p_lname, p_exercise_duration, p_history)
 	RETURNING user_id INTO o_user_id;
 	
 END;
--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,62 +1,93 @@
 // Import
-import query from '../services/db.js';
-import { UNIQUE_VIOLATION_ERROR, EMPTY_RESULT_ERROR, RAISE_EXCEPTION, SQL_ERROR_CODE } from '../configs/errors.js';
+import query from '../services/db.js'
+import {
+  UNIQUE_VIOLATION_ERROR,
+  EMPTY_RESULT_ERROR,
+  RAISE_EXCEPTION,
+  SQL_ERROR_CODE,
+} from '../configs/errors.js'
 
-const model = {};
+const model = {}
 
-model.insertSingle = async (username, email, password, fname, lname, is_highrisk, exerciseDuration, history) => {
-    // TODO: Create stored procedure
-    const sql = `
-        CALL register_user($1, $2, $3, $4, $5, $6, $7, $8, $9);
+model.insertSingle = async (
+  username,
+  email,
+  password,
+  fname,
+  lname,
+  exerciseDuration,
+  history
+) => {
+  // TODO: Create stored procedure
+  const sql = `
+        CALL register_user($1, $2, $3, $4, $5, $6, $7, $8);
     `
-    return query(sql, [username, email, password, fname, lname, true, exerciseDuration, history,null])
-        .then((result) => {
-            const { rows } = result;
+  return query(sql, [
+    username,
+    email,
+    password,
+    fname,
+    lname,
+    exerciseDuration,
+    history,
+    null,
+  ])
+    .then((result) => {
+      const { rows } = result
 
-            // If there is an error message, throw it
-            if (rows.length == 1 && result.rows[0].errMsg) {
-                throw new RAISE_EXCEPTION(500, result.rows[0].errMsg);
-            }
-            console.log("User inserted successfully"); // #DEBUG
-            return rows[0];
-        })
-        .catch((error) => {
-            if (error.code === SQL_ERROR_CODE.UNIQUE_VIOLATION) {
-                throw new UNIQUE_VIOLATION_ERROR(409, `Username or email already exists.`);
-            }
-            if (error.code === SQL_ERROR_CODE.RAISE_EXCEPTION) {
-                // Thrown from a stored procedure, etc
-                throw new RAISE_EXCEPTION(500, error.message);
-            }
+      // If there is an error message, throw it
+      if (rows.length == 1 && result.rows[0].errMsg) {
+        throw new RAISE_EXCEPTION(500, result.rows[0].errMsg)
+      }
+      console.log('User inserted successfully') // #DEBUG
+      return rows[0]
+    })
+    .catch((error) => {
+      if (error.code === SQL_ERROR_CODE.UNIQUE_VIOLATION) {
+        throw new UNIQUE_VIOLATION_ERROR(
+          409,
+          `Username or email already exists.`
+        )
+      }
+      if (error.code === SQL_ERROR_CODE.RAISE_EXCEPTION) {
+        // Thrown from a stored procedure, etc
+        throw new RAISE_EXCEPTION(500, error.message)
+      }
 
-            throw error;
-        })
+      throw error
+    })
 }
 
 model.retrieveByUsername = async (username) => {
-    const sql = `
+  const sql = `
     SELECT * FROM users
 	WHERE username = $1;
     `
-    return query(sql, [username]).then((result) => {
-        const { rows } = result;
-        if (rows.length === 0) {
-            throw new EMPTY_RESULT_ERROR(404, "User not found with the provided username.");
-        }
-        return rows[0];
-    });
+  return query(sql, [username]).then((result) => {
+    const { rows } = result
+    if (rows.length === 0) {
+      throw new EMPTY_RESULT_ERROR(
+        404,
+        'User not found with the provided username.'
+      )
+    }
+    return rows[0]
+  })
 }
 
-model.retrieveByUserID = async (userID) =>{
-    const sql = `
+model.retrieveByUserID = async (userID) => {
+  const sql = `
     SELECT * FROM users WHERE user_id = $1
     `
-    return query(sql, [userID]).then((result) => {
-    const { rows } = result;
+  return query(sql, [userID]).then((result) => {
+    const { rows } = result
     if (rows.length === 0) {
-        throw new EMPTY_RESULT_ERROR(404, "User not found with the provided username.");
+      throw new EMPTY_RESULT_ERROR(
+        404,
+        'User not found with the provided username.'
+      )
     }
-    return rows[0];
-    });
+    return rows[0]
+  })
 }
-export default model;
+export default model
